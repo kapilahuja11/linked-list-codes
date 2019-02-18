1fbeff8aa43dcdc5ccc4b2b74c6d05377f0cd4e2
kappu11 <47283826+kappu11@users.noreply.github.com>
2019-02-18 04:56:34 -0800

    Add files via upload

---
+++ delete element in unsorted list.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node* next;
+};
+struct node* root=NULL;
+
+
+void insertatend()
+{
+	struct node* temp;
+	temp=(struct node*)malloc(sizeof(struct node));
+	printf("\nenter the data:");
+	scanf("%d",&temp->data);
+	temp->next=NULL;
+	if(root==NULL)
+	{
+		root=temp;
+	}
+	else
+	{
+	  struct node* temp1;
+	  temp1=root;
+	    	while(temp1->next!=NULL)
+	    	{
+	    		temp1=temp1->next;
+			}
+			temp1->next=temp;
+			temp->next=NULL;
+		}	
+}
+void deleteduplicate()
+{
+	struct node *temp1,*temp2,*temp3;
+	temp1=root;
+	while(temp1!=NULL && temp1->next!=NULL)
+	{
+		temp2=temp1;
+		while(temp2->next!=NULL)
+		{
+			 if(temp1->data==temp2->next->data)
+			 {
+			 	temp3=temp2->next;
+			 	temp2->next=temp3->next;
+			 	temp3->next=NULL;
+			 	free(temp3);
+			 }
+			 else
+			  temp2=temp2->next;
+		}
+		temp1=temp1->next;
+	}
+	}
+	
+
+
+void display()
+{
+	struct node* temp;
+	temp=root;
+	if(root==NULL)
+	{
+	printf("\n no nodes in the list:");
+    }
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("%d->>",temp->data);
+			temp=temp->next;
+		}
+	}
+}
+
+
+int main()
+{
+	int ch,l;
+	printf("\n1.add node at the end:");
+	printf("\n2.remove duplicate element:");
+	printf("\n3.display:");
+	printf("\n4.exit:");
+	while(1)
+	{
+		printf("\nenter your choice:");
+		scanf("%d",&ch);
+	     switch(ch)
+	     {
+	     	case 1:insertatend();
+	     	  break;
+	     	case 2:deleteduplicate();
+	     	break;
+	     	case 3:display();
+	     	break;
+	       	case 4:exit(0); 
+	     	break;
+	     	default:printf("\nyou have entered wrong choice:");
+		 }
+	}
+}
