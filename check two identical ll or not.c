bafc478d34680c21099c0ea52e6ba4fb1b8f56d2
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-05-31 00:33:37 +0530

    Update check two identical ll or not.c

--- check two identical ll or not.c
+++ check two identical ll or not.c
@@ -5,25 +5,23 @@ struct node
 	int data;
 	struct node* next;
 };
-struct node* root1=NULL;
-struct node* root2=NULL;
 
 
-void linkedlist1()
+void linkedlist(struct node **root)
 {
 	struct node* temp;
 	temp=(struct node*)malloc(sizeof(struct node));
 	printf("\nenter the data:");
 	scanf("%d",&temp->data);
 	temp->next=NULL;
-	if(root1==NULL)
+	if(*root==NULL)
 	{
-		root1=temp;
+		*root=temp;
 	}
 	else
 	{
 	  struct node* temp1;
-	  temp1=root1;
+	  temp1=*root;
 	    	while(temp1->next!=NULL)
 	    	{
 	    		temp1=temp1->next;
@@ -34,30 +32,8 @@ void linkedlist1()
 }
 
 
-void linkedlist2()
-{
-	struct node* temp;
-	temp=(struct node*)malloc(sizeof(struct node));
-	printf("\nenter the data:");
-	scanf("%d",&temp->data);
-	temp->next=NULL;
-	if(root2==NULL)
-	{
-		root2=temp;
-	}
-	else
-	{
-	  struct node* temp1;
-	  temp1=root2;
-	    	while(temp1->next!=NULL)
-	    	{
-	    		temp1=temp1->next;
-			}
-			temp1->next=temp;
-			temp->next=NULL;
-		}	
-}
-int check(){
+
+int check(struct node *root1,struct node *root2){
 	struct node *temp=root1,*temp1=root2;
 	while(1)
 	{
@@ -76,6 +52,8 @@ int check(){
 int main()
 {
 	int ch,k;
+	struct node *root1=NULL;
+	struct node *root2=NULL;
 	printf("\n1.enter data in linked list one:");
 	printf("\n2.enter data in linked list two:");
 	printf("\n3.check whther two ll are identical or not:");
@@ -86,11 +64,11 @@ int main()
 		scanf("%d",&ch);
 	     switch(ch)
 	     {
-	     	case 1:linkedlist1();
+	     	case 1:linkedlist(&root1);
 	     	  break;
-	     	case 2:linkedlist2();
+	     	case 2:linkedlist(&root2);
 	     	break;
-	     	case 3:k=check();
+	     	case 3:k=check(root1,root2);
 	     	       if(k==1)
 	     	       printf("\n linked list is identical");
 	     	       else
@@ -101,9 +79,8 @@ int main()
 	     	default:printf("\nyou have entered wrong choice:");
 		 }
 	
-	}}
+	}
+}	
 	
-	
-
 
 
